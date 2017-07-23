17d7b6896ab1be586e96ee1bf8259df4d87af864
Jim Rohrer <jrohrer1@gmail.com>
2017-07-23 17:44:11 -0400

    Updated script with new lambda functions

--- lambda/remoteworkspaces/remoteworkspaces.py
+++ lambda/remoteworkspaces/remoteworkspaces.py
@@ -1,10 +1,32 @@
 import winrm
 import boto3
 import string
+import json
+
+code_pipeline = boto3.client('codepipeline')
+client = boto3.client('ssm')
+s3 = boto3.client('s3')
+ec2 = boto3.client('ec2')
+workspaces = boto3.client('workspaces')
+
+def get_workspace_public_ip(event, context):
+  if event['CodePipeline.job']:
+    input_artifact = event['CodePipeline.job']['data']['inputArtifacts']
+    s3_bucket = input_artifact['location']['s3location']['bucketName']
+    s3_key = input_artifact['location']['s3location']['objectKey']
+
+    output_json_string = s3.get_object(
+      Bucket=s3_bucket,
+      Key=s3_key
+    )
+
+    print(output_json_string['Body'].read())
 
 def install_chef(event, context):
-    client = boto3.client('ssm')
-
+    if event['CodePipeline.job']:
+      job_id = event['CodePipeline.job']['id']
+    else:
+      job_id = None
     workspaces_username = "chef"
     workspaces_ip = "34.230.188.79"
     workspaces_password = client.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
@@ -20,13 +42,23 @@ def install_chef(event, context):
 
     s = winrm.Session(workspaces_ip, auth=(workspaces_username, workspaces_password))
 
-    r = s.run_ps(command)
-    print(r.std_err)
-    print(r.std_out)
+    continue_job_later(job_id, 'Chef install started...')
+
+    response = s.run_ps(command)
+    print(response.std_err)
+    print(response.std_out)
+
+    if job_id:
+      if response.status_code == 0:
+        put_job_success(job_id, "Chef successfully installed")
+      else:
+        put_job_failure(job_id, "Chef installation failed")
 
 def execute_chef(event, context):
-    client = boto3.client('ssm')
-
+    if event['CodePipeline.job']:
+      job_id = event['CodePipeline.job']['id']
+    else:
+      job_id = None
     workspaces_username = "chef"
     workspaces_ip = "34.230.188.79"
     workspaces_password = client.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
@@ -53,7 +85,69 @@ def execute_chef(event, context):
 
     session = winrm.Session(workspaces_ip, auth=(workspaces_username, workspaces_password))
 
-    response = session.run_ps(command)
+    continue_job_later(job_id, 'Chef execution started...')
 
+    response = session.run_ps(command)
+    
     print(response.std_err)
     print(response.std_out)
+
+    if job_id:
+      if response.status_code == 0:
+        put_job_success(job_id, "Chef successfully executed")
+      else:
+        put_job_failure(job_id, "Chef execution failed")
+
+def put_job_success(job, message):
+    """Notify CodePipeline of a successful job
+    
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_success_result()
+    
+    """
+    print('Putting job success')
+    print(message)
+    code_pipeline.put_job_success_result(jobId=job)
+  
+def put_job_failure(job, message):
+    """Notify CodePipeline of a failed job
+    
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_failure_result()
+    
+    """
+    print('Putting job failure')
+    print(message)
+    code_pipeline.put_job_failure_result(jobId=job, failureDetails={'message': message, 'type': 'JobFailed'})
+ 
+def continue_job_later(job, message):
+    """Notify CodePipeline of a continuing job
+    
+    This will cause CodePipeline to invoke the function again with the
+    supplied continuation token.
+    
+    Args:
+        job: The JobID
+        message: A message to be logged relating to the job status
+        continuation_token: The continuation token
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_success_result()
+    
+    """
+    
+    # Use the continuation token to keep track of any job execution state
+    # This data will be available when a new job is scheduled to continue the current execution
+    continuation_token = json.dumps({'previous_job_id': job})
+    
+    print('Putting job continuation')
+    print(message)
+    code_pipeline.put_job_success_result(jobId=job, continuationToken=continuation_token)
