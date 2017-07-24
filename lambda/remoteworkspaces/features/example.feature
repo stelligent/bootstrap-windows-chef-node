136637b404420b918ef1b2f9f82f702b45510beb
Eric Kascic <eric.kascic@stelligent.com>
2017-07-24 16:31:36 -0400

    1. Discover the IP of the workspace instead of hardcoding 2. Streamline the gherkin for the feature 3. Make the tests a little bit more specific as far as which paths to check for proper installation

--- lambda/remoteworkspaces/features/example.feature
+++ lambda/remoteworkspaces/features/example.feature
@@ -1,6 +1,11 @@
-Feature: Showing off behave
+Feature: Workspaces are provisioned
 
-  Scenario: Run a simple test
-    Given we have behave installed
-    When we winrm into the workspace and check if Chef is installed
-    When we winrm into the workspace and check if Eclipse is installed
+  As a devops engineer
+  I would like to verify that a Workspace is provisioned correctly with an automated test
+  So that verifications are reproducible and not ad-hoc
+
+# we'd verify versions too in a more realistic scenario
+Scenario: Smoke Test for Eclipse
+  When the workspace has been provisioned
+  Then Chef is installed
+   And Eclispe is installed
--- lambda/remoteworkspaces/features/steps/example_steps.py
+++ lambda/remoteworkspaces/features/steps/example_steps.py
@@ -3,44 +3,54 @@ import winrm, base64, subprocess, sys
 import sure
 from behave import *
 
-@given('we have behave installed')
+@when(u'the workspace has been provisioned')
 def step_impl(context):
-    pass
+    context.workspace_ip = find_public_ip_of_workspace()
 
-@when('we winrm into the workspace and check if Chef is installed')
+@then('Chef is installed')
 def step_impl(context):
+    session = create_session(context.workspace_ip)
+
+    command = """
+    Test-Path C:\opscode\chef
+    """
+    r = session.run_ps(command)
+
+    actual_response = str(r.std_out.decode('ascii').replace('\r\n', ""))
+    expected_response = "True"
+
+    actual_response.should.be.equal(expected_response)
+    print(actual_response)
+
+@then('Eclispe is installed')
+def step_impl(context):
+    session = create_session(context.workspace_ip)
+
+    command = """
+    Test-Path D:\eclipse\eclipse.exe
+    """
+    r = session.run_ps(command)
+
+    actual_response = str(r.std_out.decode('ascii').replace('\r\n', ""))
+    expected_response = "True"
+
+    actual_response.should.be.equal(expected_response)
+
+def create_session(workspaces_ip):
     client = boto3.client('ssm')
 
     workspaces_username = "chef"
-    workspaces_ip = "34.195.157.83"
     workspaces_password = client.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
-
-    command = """
-    Test-Path C:\chef
-    """
     session = winrm.Session(workspaces_ip, auth=(workspaces_username, workspaces_password))
+    return session
 
-    r = session.run_ps(command)
+def find_public_ip_of_workspace():
+    ec2 = boto3.client('ec2')
+    workspaces = boto3.client('workspaces')
+    cloudformation = boto3.resource('cloudformation')
 
-    formatted_response = str(r.std_out.decode('ascii').replace('\r\n', ""))
-
-    formatted_response.should.be.equal("True")
-
-@when('we winrm into the workspace and check if Eclipse is installed')
-def step_impl(context):
-    client = boto3.client('ssm')
-
-    workspaces_username = "chef"
-    workspaces_ip = "34.195.157.83"
-    workspaces_password = client.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
-
-    command = """
-    Test-Path D:\eclipse
-    """
-    session = winrm.Session(workspaces_ip, auth=(workspaces_username, workspaces_password))
-
-    r = session.run_ps(command)
-
-    formatted_response = str(r.std_out.decode('ascii').replace('\r\n', ""))
-
-    formatted_response.should.be.equal("True")
+    stack = cloudformation.Stack('WorkspaceBuilder')
+    stack_resource = stack.Resource('workspace1').physical_resource_id
+    workspace_private_ip = workspaces.describe_workspaces(WorkspaceIds=[stack_resource])['Workspaces'][0]['IpAddress']
+    workspace_public_ip = ec2.describe_network_interfaces(Filters=[{ 'Name' : 'private-ip-address', 'Values' : [workspace_private_ip] }])['NetworkInterfaces'][0]['Association']['PublicIp']
+    return workspace_public_ip
