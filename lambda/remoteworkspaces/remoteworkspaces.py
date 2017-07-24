fa26195ca5de37d850aa858249ace2f1daf30bbc
Brian Jakovich <brian.jakovich@stelligent.com>
2017-07-24 01:35:19 -0400

    Moved boto into lambda dir... lambda's internal boto is not up-to-date with ssm. Moved lambdas out of vpcs. Was causing failures

--- lambda/remoteworkspaces/remoteworkspaces.py
+++ lambda/remoteworkspaces/remoteworkspaces.py
@@ -9,6 +9,7 @@ client = boto3.client('ssm')
 s3 = boto3.client('s3')
 ec2 = boto3.client('ec2')
 workspaces = boto3.client('workspaces')
+cloudformation = boto3.resource('cloudformation')
 
 def get_workspace_public_ip(event, context):
   if event['CodePipeline.job']:
@@ -23,8 +24,6 @@ def get_workspace_public_ip(event, context):
 
     print(output_json_string['Body'].read())
 
-
-
 def execute_tests(event, context):
   behave_main("features/example.feature")
 
@@ -35,7 +34,7 @@ def install_chef(event, context):
     else:
       job_id = None
     workspaces_username = "chef"
-    workspaces_ip = "34.230.188.79"
+    workspaces_ip = find_public_ip_of_workspace()
     workspaces_password = client.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
     access_key = client.get_parameter(Name='snhu-workspace-user-access-key', WithDecryption=True)['Parameter']['Value']
     secret_access_key = client.get_parameter(Name='snhu-workspace-user-secret-key', WithDecryption=True)['Parameter']['Value']
@@ -53,7 +52,7 @@ def install_chef(event, context):
 
     response = s.run_ps(command)
     print(response.std_err)
-    print(response.std_out)
+    print(response.std_out.decode('ascii').replace('\r\n', "\n"))
 
     if job_id:
       if response.status_code == 0:
@@ -67,7 +66,7 @@ def execute_chef(event, context):
     else:
       job_id = None
     workspaces_username = "chef"
-    workspaces_ip = "34.230.188.79"
+    workspaces_ip = find_public_ip_of_workspace()
     workspaces_password = client.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
     access_key = client.get_parameter(Name='snhu-workspace-user-access-key', WithDecryption=True)['Parameter']['Value']
     secret_access_key = client.get_parameter(Name='snhu-workspace-user-secret-key', WithDecryption=True)['Parameter']['Value']
@@ -97,7 +96,7 @@ def execute_chef(event, context):
     response = session.run_ps(command)
 
     print(response.std_err)
-    print(response.std_out)
+    print(response.std_out.decode('ascii').replace('\r\n', "\n"))
 
     if job_id:
       if response.status_code == 0:
@@ -158,3 +157,10 @@ def continue_job_later(job, message):
     print('Putting job continuation')
     print(message)
     code_pipeline.put_job_success_result(jobId=job, continuationToken=continuation_token)
+
+def find_public_ip_of_workspace():
+    stack = cloudformation.Stack('WorkspaceBuilder')
+    stack_resource = stack.Resource('workspace1').physical_resource_id
+    workspace_private_ip = workspaces.describe_workspaces(WorkspaceIds=[stack_resource])['Workspaces'][0]['IpAddress']
+    workspace_public_ip = ec2.describe_network_interfaces(Filters=[{ 'Name' : 'private-ip-address', 'Values' : [workspace_private_ip] }])['NetworkInterfaces'][0]['Association']['PublicIp']
+    return workspace_public_ip
