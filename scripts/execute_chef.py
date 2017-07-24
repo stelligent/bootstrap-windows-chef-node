410fa722796eebe9351073aee2527f8095d67bf5
Jim Rohrer <jrohrer1@gmail.com>
2017-07-24 12:33:22 -0400

    Some fixes to the python scripts

--- scripts/execute_chef.py
+++ scripts/execute_chef.py
@@ -8,36 +8,37 @@ ec2 = boto3.client('ec2')
 workspaces = boto3.client('workspaces')
 cloudformation = boto3.resource('cloudformation')
 
-workspaces_username = "chef"
-workspaces_ip = find_public_ip_of_workspace()
-workspaces_password = ssm.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
-access_key = ssm.get_parameter(Name='snhu-workspace-user-access-key', WithDecryption=True)['Parameter']['Value']
-secret_access_key = ssm.get_parameter(Name='snhu-workspace-user-secret-key', WithDecryption=True)['Parameter']['Value']
-chef_path = "C:\chef"
-cookbooks_path = "C:\chef\cookbooks"
-cookbooks_bucket = "snhu-chef"
+def execute_chef():
+    workspaces_username = "chef"
+    workspaces_ip = find_public_ip_of_workspace()
+    workspaces_password = ssm.get_parameter(Name='chef-winrm-password', WithDecryption=True)['Parameter']['Value']
+    access_key = ssm.get_parameter(Name='snhu-workspace-user-access-key', WithDecryption=True)['Parameter']['Value']
+    secret_access_key = ssm.get_parameter(Name='snhu-workspace-user-secret-key', WithDecryption=True)['Parameter']['Value']
+    chef_path = "C:\chef"
+    cookbooks_path = "C:\chef\cookbooks"
+    cookbooks_bucket = "snhu-chef"
 
-command ="""
-Set-ExecutionPolicy RemoteSigned -force
-Import-Module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1"
-Set-AWSCredential -AccessKey %(access_key)s -SecretKey %(secret_access_key)s -StoreAs default
-if(!(Test-Path "%(chef_path)s")) {
-  New-Item "%(chef_path)s" -ItemType Directory -Force
-}
-if(!(Test-Path "%(cookbooks_path)s")) {
-  New-Item "%(cookbooks_path)s" -ItemType Directory -Force
-}
-Remove-Item -path ("%(cookbooks_path)s" + "\*") -recurse
-Read-S3Object -BucketName "%(cookbooks_bucket)s" -KeyPrefix "cookbooks" -Folder "%(cookbooks_path)s"
-chef-solo -o snhu-eclipse
-""" % locals()
+    command ="""
+    Set-ExecutionPolicy RemoteSigned -force
+    Import-Module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1"
+    Set-AWSCredential -AccessKey %(access_key)s -SecretKey %(secret_access_key)s -StoreAs default
+    if(!(Test-Path "%(chef_path)s")) {
+      New-Item "%(chef_path)s" -ItemType Directory -Force
+    }
+    if(!(Test-Path "%(cookbooks_path)s")) {
+      New-Item "%(cookbooks_path)s" -ItemType Directory -Force
+    }
+    Remove-Item -path ("%(cookbooks_path)s" + "\*") -recurse
+    Read-S3Object -BucketName "%(cookbooks_bucket)s" -KeyPrefix "cookbooks" -Folder "%(cookbooks_path)s"
+    chef-solo -o snhu-eclipse
+    """ % locals()
 
-session = winrm.Session(workspaces_ip, auth=(workspaces_username, workspaces_password))
+    session = winrm.Session(workspaces_ip, auth=(workspaces_username, workspaces_password))
 
-response = session.run_ps(command)
+    response = session.run_ps(command)
 
-print(response.std_err)
-print(response.std_out.decode('ascii').replace('\r\n', "\n"))
+    print(response.std_err)
+    print(response.std_out.decode('ascii').replace('\r\n', "\n"))
 
 def find_public_ip_of_workspace():
     stack = cloudformation.Stack('WorkspaceBuilder')
@@ -45,3 +46,5 @@ def find_public_ip_of_workspace():
     workspace_private_ip = workspaces.describe_workspaces(WorkspaceIds=[stack_resource])['Workspaces'][0]['IpAddress']
     workspace_public_ip = ec2.describe_network_interfaces(Filters=[{ 'Name' : 'private-ip-address', 'Values' : [workspace_private_ip] }])['NetworkInterfaces'][0]['Association']['PublicIp']
     return workspace_public_ip
+
+execute_chef()
--- scripts/tests.py
+++ scripts/tests.py
@@ -6,3 +6,5 @@ from behave.__main__ import main as behave_main
 
 def execute_tests():
   behave_main("features/example.feature")
+
+execute_tests()
